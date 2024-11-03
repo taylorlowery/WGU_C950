9aadd6dfc08c00b2138c0e06bab2f99b40c08b1b
Taylor Lowery <taylorlowery07@gmail.com>
2024-11-02 21:43:54 -0700

    reformat with black

--- tests/test_delivery_algorithm.py
+++ tests/test_delivery_algorithm.py
@@ -3,28 +3,34 @@ from datetime import datetime, time
 from lib.delivery_algorithm import print_package_status_at_provided_time
 from models.package import Package, DeliveryStatus
 
+
 @pytest.fixture
 def sample_package():
     """Simulate package after algorithm runs.
-    
+
     time_delivered will be populated and delivery status will be DELIVERED
     """
-    package = Package(1, "Test Address", "City", "State", "12345", time(9, 0), "10", "Notes")
+    package = Package(
+        1, "Test Address", "City", "State", "12345", time(9, 0), "10", "Notes"
+    )
     package.delivery_status = DeliveryStatus.DELIVERED
     package.time_loaded_onto_truck = time(8, 0)
     package.time_delivered = time(10, 0)
     package.truck_id = 1
     return package
 
+
 def test_package_at_hub(sample_package):
     current_time = time(7, 0)  # Before loading time
     result = print_package_status_at_provided_time(sample_package, current_time)
     assert f"Package 1 {DeliveryStatus.AT_HUB}" in result
 
+
 def test_package_en_route(sample_package):
     current_time = time(9, 0)  # En route (already picked up, before delivery)
     result = print_package_status_at_provided_time(sample_package, current_time)
     assert f"Package 1 - {DeliveryStatus.EN_ROUTE} on truck 1" in result
+
 
 def test_package_delivered(sample_package):
     current_time = time(11, 0)  # After delivery
